db0ac4fac272fdd1727fc3244680739fe5f8a6d9
Abdul Wahid <111135216+abdulwahid4423@users.noreply.github.com>
2022-09-16 06:33:43 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,138 @@
+// chapter 21 to30
+
+// var a = prompt("Enter your city")
+// var b = a.toUpperCase
+// if(a==="karachi"){
+//    alert("your city hes been found ")
+
+//    }
+//    else{
+//     alert("wrong")
+
+//    }
+
+
+// var a = prompt("Enter your favorite mobile model")
+// var arr = ["iphone 14","infex hot 10i","samsung A20",]
+//  for(a = 0 ; a < arr.length ; a++ )
+//  if(arr[a]=== "iphone 14")
+//  alert("mend in jumpan" )
+
+// if(arr[a]=== "infex hot 10i")
+//  alert("mend in pakisten" ) 
+
+// if(arr[a]=== "samaung A20")
+//  alert("mend in jumpan" ) 
+
+// if(arr[a]=== "iphone 14")
+//  alert("mend in jumpan" ) 
+
+//  var a ="pakistan"
+//  var b = a.indexOf("t")
+// alert("the word found in " + b + "") 
+
+// var a ="islamabad"
+// var b = a.slice(0,5) 
+// var c = b.replace ("islam","Hyder") 
+
+// alert(c+"abad")
+
+// var massege = "Ali and Sami are best friends.They play cricket and football together "
+// var replace = massege.replace(/and/g,"&")
+// alert(replace)
+
+// var a = 3.4514
+// var d = Math.round(a)
+// alert(d)
+
+// var headuser = prompt("Enter Head user Name");
+// var tailuser = prompt("Enter Tail user Name");
+
+// var toss = Math.random()*2;
+// var floor = Math.floor(toss)
+// if(floor===0){
+//     alert("Head" + headuser + "win the toss")
+// }else{
+//     alert("Tail" + tailuser + "Win the toss")
+// }
+
+// var a = "50"
+// var b = Number(a)
+// alert(a)   
+
+//  var a = "50"
+// var b = Number(a)
+// alert(a)   
+
+// var a = 89.555
+// var b = a.toFixed(2)
+// alert(b) 
+
+// chapet31to40
+
+// var a = new Date ()
+// console.log(a)
+
+// var a = new Date ()
+// a.setMonth(6)
+// a.setDate(5)
+// a.setFullYear(2015)
+// a.setHours(22)
+// a.setMinutes(18)
+// a.setSeconds(11)
+// console.log(a)
+
+// function abc (c,d){
+//     return c + d  
+// }
+// console.log (abc (3,5))
+// console.log (abc(6,5))
+// console.log (abc(3,6))
+
+// var a = new Date ()
+
+// console.log(a.getDay()) 
+
+// var currentdate = new Date ("1-1-2001")
+// var todaydate = new Date ();
+//  currentdate.setDate(21)
+//  var currentdate = currentdate.getTime ();
+//  var todayTime = todaydate.getTime();
+//  var diff = todayTime - currentdate
+//  var finaldate = diff/(1000 * 60 * 60 * 24 * 365)
+//  alert(finaldate)
+ 
+// var name = prompt("Enter your Name")
+// var Month = ("Enter current month") 
+// var unitno = prompt("Enter unit on")
+// var paymentdate = prompt("Enter your payment date")
+//  var unitcharges= 4.5;
+//  var dueadate = 4.8 * unitcharges
+//  if(
+//     paymentdate > 15
+//  ){
+//     alert(unitno)
+//  }
+//  else{
+//     alert(calaculate)
+//  }
+
+
+// function abc (){
+
+// var a = new Date ()
+// a.setMonth(6)
+// a.setDate(5)
+// a.setFullYear(2015)
+// a.setHours(22)
+// a.setMinutes(18)
+// a.setSeconds(11)
+// console.log(a)
+// }
+// abc()
+// function abc (greet){
+//     alert(greet)
+// }
+// abc ("Hello word")
+ 
+ 
